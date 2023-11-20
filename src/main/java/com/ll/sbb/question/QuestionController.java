fa0a1766bb868470ec22dc2f5fca74e5263487c2
hagd <hagd0520@gmail.com>
2023-11-20 22:01:48 +0900

    2-09 서비스

--- src/main/java/com/ll/sbb/question/QuestionController.java
+++ src/main/java/com/ll/sbb/question/QuestionController.java
@@ -14,11 +14,11 @@ import java.util.List;
 @Controller
 public class QuestionController {
 
-    private final QuestionRepository questionRepository;
+    private final QuestionService questionService;
 
     @GetMapping("/question/list")
     public String list(Model model) {
-        List<Question> questionList = this.questionRepository.findAll();
+        List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
     }
---
+++ src/main/java/com/ll/sbb/question/QuestionService.java
@@ -0,0 +1,16 @@
+package com.ll.sbb.question;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class QuestionService {
+    private final QuestionRepository questionRepository;
+
+    public List<Question> getList() {
+        return this.questionRepository.findAll();
+    }
+}
